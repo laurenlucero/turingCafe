6e8d3e3f7349df5b8eb71cb49afcc1f7f06d7f0d
“Lauren <lucerolaurenm@gmail.com>
2020-05-21 13:52:33 -0400

    Attempt POST, style form

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,6 +1,5 @@
 export const fetchReservations = async () => {
   let response = await fetch("http://localhost:3001/api/v1/reservations");
   let data = await response.json();
-  console.log(data);
   return data;
 };
